2288e3d9584842e930dd723bba59524ce8ca1e55
Carlo Maria Cardí <112445885+MathsAnalysis@users.noreply.github.com>
2024-03-28 02:21:55 +0100

    d

--- src/main/java/me/mathanalysis/it/uhc/config/WorldConfig.java
+++ src/main/java/me/mathanalysis/it/uhc/config/WorldConfig.java
@@ -14,36 +14,16 @@ public class WorldConfig extends CMFile {
     @Override
     public void loadDefaults() {
 
-        addSection("Normal World Settings");
-        addComment("Normal World Settings", "This is the settings for the normal world");
-        addDefault("Worlds.uhc_world.name", "uhc_world");
-        addDefault("Worlds.uhc_world.type", "NORMAL");
-        addDefault("Worlds.uhc_world.center.x", "0");
-        addDefault("Worlds.uhc_world.center.z", "0");
-        addDefault("Worlds.uhc_world.size", "1000");
+        addDefault("Worlds.World1", "World1");
+        addDefault("test", "test1", "Testing 1, testing 2!");
 
-        addSection("Practice World Settings");
-        addComment("Practice World Settings", "This is the settings for the practice world");
-        addDefault("Worlds.uhc_practice.name", "uhc_practice");
-        addDefault("Worlds.uhc_practice.type", "NORMAL");
-        addDefault("Worlds.uhc_practice.center.x", "0");
-        addDefault("Worlds.uhc_practice.center.z", "0");
-        addDefault("Worlds.uhc_practice.size", "1000");
+        addSection("Important Stuff");
+        addComment("Important Stuff", "This is the important stuff");
+        addComment("World", "This is the nether world");
+        addDefault("Nether.World", "World_nether");
+        addDefault("Nether.Environment", "NETHER");
+        addDefault("Nether.Seed", 123456789);
 
-        addSection("Nether World Settings");
-        addComment("Nether World Settings", "This is the settings for the nether world");
-        addDefault("Worlds.uhc_nether.name", "uhc_nether");
-        addDefault("Worlds.uhc_nether.type", "NETHER");
-        addDefault("Worlds.uhc_nether.center.x", "0");
-        addDefault("Worlds.uhc_nether.center.z", "0");
-        addDefault("Worlds.uhc_nether.size", "500");
 
-        addSection("End World Settings");
-        addComment("End World Settings", "This is the settings for the end world");
-        addDefault("Worlds.uhc_end.name", "uhc_end");
-        addDefault("Worlds.uhc_end.type", "END");
-        addDefault("Worlds.uhc_end.center.x", "0");
-        addDefault("Worlds.uhc_end.center.z", "0");
-        addDefault("Worlds.uhc_end.size", "100");
     }
 }
